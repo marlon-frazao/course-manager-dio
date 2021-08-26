e8a69cb05b758b2c5fa40cd10d3b12c8b9355088
marlon-frazao <frazao.marlon@gmail.com>
2021-08-26 14:15:11 -0300

    AppPipeModule and StarModule created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { StarComponent } from './star/star.component';
+import { StarComponent } from './shared/component/star/star.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './404/error-404.component';
